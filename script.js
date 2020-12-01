b41dc9a01ba85b222ad416c0ffcfe92727f22f17
Share1Radio <66054608+Share1Radio@users.noreply.github.com>
2020-12-02 00:33:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementByClassName('toggle')[0]
-const navbarlinks = document.getElementByClassName('navbarlinks')[0]
-
-toggle.addEventsListener('click',() => {
-  navbarlinks.classList.toggle('active')
-  })
